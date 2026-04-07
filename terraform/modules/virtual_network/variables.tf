eca1176bcb51678fc996c184c1cdd33f05d2aa33
De Xing Teoh <dexing30100@gmail.com>
2026-04-07 22:13:54 +1000

    add virtual network

---
+++ terraform/modules/virtual_network/variables.tf
@@ -0,0 +1,23 @@
+variable "name" {
+  default     = "vnet-global360-task"
+  type        = string
+  description = "Name of the virtual network"
+}
+
+variable "location" {
+  default     = "australiaeast"
+  type        = string
+  description = "The Azure region to deploy into"
+}
+
+variable "resource_group_name" {
+  default     = "rg-global360-task"
+  type        = string
+  description = "The name of the resource group in which to create the virtual network. Changing this forces a new resource to be created"
+}
+
+variable "address_space" {
+  default     = ["10.0.0.0/16"]
+  type        = list
+  description = "The address space that is used the virtual network. You can supply more than one address space"
+}
