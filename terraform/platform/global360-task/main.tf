a98b5c2d60e80d93612c769777539934a15621cf
De Xing Teoh <dexing30100@gmail.com>
2026-04-07 21:42:33 +1000

    add vars for rg module

--- terraform/platform/global360-task/main.tf
+++ terraform/platform/global360-task/main.tf
@@ -1,6 +1,6 @@
 module "resource_group" {
   source = "../../modules/resource_group"
 
-  name     = "rg-webapp"
-  location = "australiaeast"
+  name     = var.rg_name
+  location = var.location
 }
