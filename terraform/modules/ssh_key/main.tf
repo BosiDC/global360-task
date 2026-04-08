462fe74f92388d887a35dc9a6e693bae337495c9
De Xing Teoh <dexing30100@gmail.com>
2026-04-08 23:17:22 +1000

    add tags to ssh key

--- terraform/modules/ssh_key/main.tf
+++ terraform/modules/ssh_key/main.tf
@@ -1,3 +1,11 @@
+resource "azapi_resource" "ssh_public_key" {
+  type      = "Microsoft.Compute/sshPublicKeys@2022-11-01"
+  name      = "sshkey-global360-task"
+  location  = var.location
+  parent_id = var.parent_id
+  tags      = var.tags
+}
+
 resource "azapi_resource_action" "ssh_public_key_gen" {
   type        = "Microsoft.Compute/sshPublicKeys@2022-11-01"
   resource_id = azapi_resource.ssh_public_key.id
@@ -5,11 +13,4 @@ resource "azapi_resource_action" "ssh_public_key_gen" {
   method      = "POST"
 
   response_export_values = ["publicKey", "privateKey"]
-}
-
-resource "azapi_resource" "ssh_public_key" {
-  type      = "Microsoft.Compute/sshPublicKeys@2022-11-01"
-  name      = "sshkey-global360-task"
-  location  = var.location
-  parent_id = var.parent_id
 }
