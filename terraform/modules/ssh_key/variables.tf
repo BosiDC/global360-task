2b8154c0214396a1513575a1dba3084923412d75
De Xing Teoh <dexing30100@gmail.com>
2026-04-08 14:52:54 +1000

    add ssh_key gen for VMs

---
+++ terraform/modules/ssh_key/variables.tf
@@ -0,0 +1,12 @@
+
+
+variable "rg_id" {
+  type        = string
+  description = "ID of the resouce group"
+}
+
+variable "location" {
+  default     = "australiaeast"
+  type        = string
+  description = "The Azure region to deploy into"
+}
---
+++ terraform/modules/ssh_key/versions.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    azurerm = {
+      version = "~> 4.67.0"
+    }
+    azapi = {
+      version = "~> 2.9.0"
+    }
+    random = {
+      version = "~> 3.8.1"
+    }
+  }
+
+  required_version = "~> 1.14.8"
+}
